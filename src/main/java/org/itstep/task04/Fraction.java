fdb4e0f76277e2a5bb729bf72a4bfc1c71468c2a
shabrat2015 <shabrat2015@gmail.com>
2020-10-26 13:50:50 +0200

    Z4

---
+++ src/main/java/org/itstep/task04/Fraction.java
@@ -0,0 +1,21 @@
+package org.itstep.task04;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    Fraction()
+    {
+
+    }
+    Fraction(int numerator,int denominator)
+    {
+        this.numerator=numerator;
+        this.denominator=denominator;
+    }
+
+    public int getNumerator(){return numerator; }
+    public void setNumerator(int numerator){this.numerator=numerator; }
+    public int getDenominator(){return denominator; }
+    public void setDenominator(int denominator){this.denominator=denominator; }
+}
