5840f716f5ee64a07f48ab0bd44492b750ee37af
shabrat2015 <shabrat2015@gmail.com>
2020-10-26 16:47:23 +0200

    Z4  good

--- src/main/java/org/itstep/task04/Fraction.java
+++ src/main/java/org/itstep/task04/Fraction.java
@@ -3,6 +3,7 @@ package org.itstep.task04;
 public class Fraction {
     private int numerator;
     private int denominator;
+    //public int num1,  den1,  num2,  den2,  rnum,  rden;
 
     Fraction()
     {
@@ -18,4 +19,32 @@ public class Fraction {
     public void setNumerator(int numerator){this.numerator=numerator; }
     public int getDenominator(){return denominator; }
     public void setDenominator(int denominator){this.denominator=denominator; }
+
+
+    Fraction subtraction(Fraction f)
+    {
+        if(this.numerator -f.numerator==f.denominator)
+        { return new Fraction((this.numerator/f.denominator),this.denominator/f.denominator);}
+        else{
+            return new Fraction((this.numerator -f.numerator),this.denominator= f.denominator);}
+    }
+    Fraction addition(Fraction f)
+    {
+        if(this.numerator +f.numerator==f.denominator)
+        { return new Fraction((this.numerator),this.denominator/f.denominator);}
+        else{
+        return new Fraction((this.numerator +f.numerator),this.denominator= f.denominator);}
+    }
+    Fraction division(Fraction f)
+    { if(this.denominator/this.denominator==1)
+    { return new Fraction((this.numerator),this.denominator=f.numerator);}
+    else{
+        return new Fraction((this.numerator*f.numerator ),f.denominator/ this.denominator);}
+
+    }
+    Fraction multiplication(Fraction f)
+    {
+        return new Fraction((this.numerator * f.numerator),this.denominator* f.denominator);
+    }
+
 }
